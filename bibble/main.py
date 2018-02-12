eb60256afaac2011c967c0ac7e7ced87b24b94f2
Bryan Lawrence <bryannlawrence@gmail.com>
2018-02-12 19:05:21 +0000

    Support for DOIs, unpublished, and some elements of biblatex, and better latex garbage removal

--- bibble/main.py
+++ bibble/main.py
@@ -18,6 +18,11 @@ _months = {
 def _author_fmt(author):
     return u' '.join(author.first() + author.middle() + author.last())
 
+def _unlatex(string):
+    """ Remove curly braces and other latex guff that we don't want for html output"""
+    for x,y in [('{',''),('}',''),('\&','&')]:
+        string = string.replace(x,y)
+    return string
 
 def _andlist(ss, sep=', ', seplast=', and ', septwo=' and '):
     if len(ss) <= 1:
@@ -34,14 +39,22 @@ def _author_list(authors):
 
 def _venue_type(entry):
     venuetype = ''
+    f = entry.fields
     if entry.type == 'inbook':
         venuetype = 'Chapter in '
     elif entry.type == 'techreport':
         venuetype = 'Technical Report '
     elif entry.type == 'phdthesis':
-        venuetype = 'Ph.D. thesis, {}'.format(entry.fields['school'])
+        venuetype = 'Ph.D. thesis, {}'.format(f['school'])
     elif entry.type == 'mastersthesis':
-        venuetype = 'Master\'s thesis, {}'.format(entry.fields['school'])
+        venuetype = 'Master\'s thesis, {}'.format(f['school'])
+    elif entry.type == 'unpublished':
+        if 'howpublished' in f: 
+            venuetype='Unpublished {}. '.format(_unlatex(f['howpublished']))
+        elif 'type' in f:
+            venuetype='Unpublished (type={}). '.format(f['type'])
+        else:
+            venuetype='Unpublished. '
     return venuetype
 
 
@@ -49,14 +62,20 @@ def _venue(entry):
     f = entry.fields
     venue = ''
     if entry.type == 'article':
-        venue = f['journal']
+        # biblatex uses journaltitle instead of journal
+        if 'journal' in f:
+            venue = f['journal']
+        elif 'journaltitle' in f:
+            venue = f['journaltitle']
+        else:
+            raise ValueError('No valid journal title found for article')
         try:
             if f['volume'] and f['number']:
                 venue += ' {0}({1})'.format(f['volume'], f['number'])
         except KeyError:
             pass
     elif entry.type == 'inproceedings':
-        venue = f['booktitle']
+        venue = _unlatex(f['booktitle'])
         try:
             if f['series']:
                 venue += ' ({})'.format(f['series'])
@@ -68,8 +87,11 @@ def _venue(entry):
         venue = '{0}, {1}'.format(f['number'], f['institution'])
     elif entry.type == 'phdthesis' or entry.type == 'mastersthesis':
         venue = ''
-    else:
-        venue = 'Unknown venue (type={})'.format(entry.type)
+    elif entry.type == 'unpublished':
+        if 'eventtitle' in f:
+           venue = _unlatex(f['eventtitle'])
+        else:
+            venue = ''
     return venue
 
 
@@ -78,10 +100,7 @@ def _title(entry):
         title = entry.fields['chapter']
     else:
         title = entry.fields['title']
-
-    # remove curlies from titles -- useful in TeX, not here
-    title = title.replace('{','').replace('}','')
-    return title
+    return _unlatex(title)
 
 
 def _main_url(entry):
@@ -90,6 +109,13 @@ def _main_url(entry):
         if f in entry.fields:
             return entry.fields[f]
     return None
+
+
+def _doi(entry):
+    if 'doi' in entry.fields:
+        return entry.fields['doi']
+    else:
+        return ''
 
 
 def _extra_urls(entry):
@@ -117,7 +143,7 @@ def _month_match(mon):
 def _month_name(monthnum):
     try:
         return month_name[int(monthnum)]
-    except Exception, e:
+    except (ValueError, KeyError):
         return ''
 
 
@@ -148,6 +174,7 @@ def main(bibfile, template, output):
     tenv.filters['main_url'] = _main_url
     tenv.filters['extra_urls'] = _extra_urls
     tenv.filters['monthname'] = _month_name
+    tenv.filters['doi'] = _doi
     tmpl = tenv.from_string(template.read())
 
     # Parse the BibTeX file.
@@ -156,11 +183,19 @@ def main(bibfile, template, output):
     # Include the bibliography key in each entry.
     for k, v in db.entries.items():
         v.fields['key'] = k
+        # handle biblatex dates 
+        if 'year' not in v.fields:
+            if 'date' not in v.fields:
+                raise ValueError('No valid date for %s' % k)
+            else:
+                v.fields['year'] = v.fields['date'][0:4]
+                if len(v.fields['date'])>=7:
+                    v.fields['month']=v.fields['date'][5:7]
 
     # Render the template.
     bib_sorted = sorted(db.entries.values(), key=_sortkey, reverse=True)
     out = tmpl.render(entries=bib_sorted)
-    print(out)
+    print (out)
 
 
 if __name__ == '__main__':
